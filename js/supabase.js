e5cbb27e262859aec84aa0f02b712764b151820f
azlan_data_my <muhammadazlankhamis@gmail.com>
2026-07-15 11:26:22 +0800

    Fix script all

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // ===== TRACKPO — SUPABASE CONFIG =====
 const SUPABASE_URL = 'https://dhyddyhbpmzstbhrsgrq.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_krPrYN9no79aBlSbKiXsmw_3VuyxrN';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoeWRkeWhicG16c3RiaHJzZ3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MDg5MDMsImV4cCI6MjA5OTQ4NDkwM30.czIG6b_zWhZlFj3MNfFVv6Yb65cL5DD_7yLmV37rFCM';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
